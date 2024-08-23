4c698940ac6b52b81b23f7190c4eaa598398bb8b
cedricHOTZ <hotz.cedric@gmail.com>
2024-08-23 14:09:46 +0200

    versel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+// import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 import db from "@astrojs/db";
 import dotenv from 'dotenv';
 
-dotenv.config();
+  dotenv.config();
 
 
 // https://astro.build/config
 export default defineConfig({
 //SSR
 output: 'server',
-  adapter: node({
-    mode: 'standalone', // Spécifiez le mode ici
-  }),
-  integrations: [db()]
+ adapter: vercel(),
+   integrations: [db()]
 });
--- dist/server/chunks/_astro_db_Duvznnk9.mjs
+++
@@ -1,8 +0,0 @@
-import { normalizeDatabaseUrl, createLocalDatabaseClient, asDrizzleTable } from '@astrojs/db/runtime';
-import '@astrojs/db/dist/runtime/virtual.js';
-
-const dbUrl = normalizeDatabaseUrl(process.env.ASTRO_DATABASE_FILE, "file:///Users/ced/Desktop/astro-db1/.astro/content.db");
-const db = createLocalDatabaseClient({ dbUrl });
-const Meal = asDrizzleTable("Meal", { "columns": { "id": { "type": "number", "schema": { "unique": false, "deprecated": false, "name": "id", "collection": "Meal", "primaryKey": true } }, "title": { "type": "text", "schema": { "unique": false, "deprecated": false, "name": "title", "collection": "Meal", "primaryKey": false, "optional": false } }, "description": { "type": "text", "schema": { "unique": false, "deprecated": false, "name": "description", "collection": "Meal", "primaryKey": false, "optional": false } }, "image": { "type": "text", "schema": { "unique": false, "deprecated": false, "name": "image", "collection": "Meal", "primaryKey": false, "optional": false } }, "imageAlt": { "type": "text", "schema": { "unique": false, "deprecated": false, "name": "imageAlt", "collection": "Meal", "primaryKey": false, "optional": false } }, "published": { "type": "date", "schema": { "optional": false, "unique": false, "deprecated": false, "name": "published", "collection": "Meal" } }, "authorName": { "type": "text", "schema": { "unique": false, "deprecated": false, "name": "authorName", "collection": "Meal", "primaryKey": false, "optional": false } } }, "deprecated": false, "indexes": {} }, false);
-
-export { Meal as M, db as d };
